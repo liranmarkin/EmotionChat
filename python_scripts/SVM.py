c8e4b1fd42e3df6715729f1e52a0c69f63c37de1
Liran Markin <liran.markin@gmail.com>
2017-02-26 13:42:46 +0200

    the way to fitting to model

--- python_scripts/SVM.py
+++ python_scripts/SVM.py
@@ -77,7 +77,7 @@ class SVM():
         b_tmp = y - np.dot(w.T, X.T)
         return np.mean(b_tmp)
     def calc_w(self, alpha, y, X):
-        return np.dot(alpha * y, X)
+        return np.dot(np.dot(alpha , y), X)
     # Prediction
     def h(self, X, w, b):
         return np.sign(np.dot(w.T, X.T) + b).astype(int)
--- python_scripts/myscript.py
+++ python_scripts/myscript.py
@@ -1,173 +1,56 @@
-import os, re
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.ensemble import RandomForestClassifier
+import os
+
 import pandas as pd
 import numpy as np
-from nltk.corpus import stopwords
+from BagOfWords import get_features
 from SVM import SVM
 
 
-
-def read_data():
-    return pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'real_data.csv'))
+def read_data(path):
+    return pd.read_csv(os.path.join(os.path.dirname(__file__), path))
 
 def get_sentences(data):
-    return data['sentence'].values.tolist()
+    return data['sentence'].values
 
 def get_emotions(data):
-    return data['emotion'].values.tolist()
-
-
-
-
-def sentence_to_wordlist(sentence, remove_stopwords=False):
-    # Function to convert a document to a sequence of words,
-    # optionally removing stop words.  Returns a list of words.
-    #
-    # 1. Remove non-letters
-    sentence = re.sub("[^a-zA-Z]", " ", sentence)
-    #
-    # 2. Convert words to lower case and split them
-    words = sentence.lower().split()
-    #
-    # 3. Optionally remove stop words (false by default)
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in stops]
-    #
-    # 4. Return a list of words
-    return words
-
-
-
-def clean_sentences(sentences):
-    # Initialize an empty list to hold the clean_train
-    clean = []
-
-    for i in xrange(0, len(train['content'])):
-        clean.append(" ".join(sentence_to_wordlist(sentences[i], True)))
-
-    return clean
-
-def get_features(clean_sentences):
-
-    # Initialize the "CountVectorizer" object, which is scikit-learn's
-    # bag of words toolsentiment.
-    vectorizer = CountVectorizer(analyzer="word", \
-                                 tokenizer=None, \
-                                 preprocessor=None, \
-                                 stop_words=None, \
-                                 max_features=5000)
-
-    # fit_transform() does two functions: First, it fits the model
-    # and learns the vocabulary; second, it transforms our training data
-    # into feature vectors. The input to fit_transform should be a list of
-    # strings.
-    train_data_features = vectorizer.fit_transform(clean_sentences)
-
-    # Numpy arrays are easy to work with, so convert the result to an
-    # array
-    train_data_features = train_data_features.toarray()
+    return data['emotion'].values
 
 if __name__ == '__main__':
-    train = read_data()
+
+    print "Reading data from file"
+
+    train = read_data('./data/real_data.csv')
+
+    train = train[:100]
 
     sentences = get_sentences(train)
     emotions = get_emotions(train)
 
-    print sentences
-    print emotions
+    #Transforming emotions to integer values
+    emotion_list = list(set(emotions))
+    emotion_numbers = {}
+    for i in xrange(len(emotion_list)):
+        emotion_numbers[emotion_list[i]] = i
 
+    print "Press Enter to Continue..."
     raw_input()
 
-    # ******* Train a random forest using the bag of words
-    #
-    print "Training the random forest (this may take a while)..."
+    # Train a SVM using the bag of words
+    print "Training the SVM (this may take a while)..."
 
-    # Initialize a Random Forest classifier with 100 trees
-    forest = RandomForestClassifier(n_estimators=100)
+    # Initialize a SVM classifier
+    model = SVM(max_iter=100, kernel_type='linear', C=1.0, epsilon=0.001)
 
-    # Fit the forest to the training set, using the bag of words as
+    X = get_features(sentences)
+    y = np.vectorize(emotion_numbers.get)(emotions)
+
+    print X
+    print y
+    # Fit the SVM to the training set, using the bag of words as
     # features and the sentiment labels as the response variable
     #
     # This may take a few minutes to run
-    forest = forest.fit(train_data_features, train["sentiment"])
-
-    # Create an empty list and append the clean reviews one by one
-    clean_test_reviews = []
-
-    print "Cleaning and parsing the test set movie reviews...\n"
-    for i in xrange(0, len(test["review"])):
-        clean_test_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], True)))
-
-    # Get a bag of words for the test set, and convert to a numpy array
-    test_data_features = vectorizer.transform(clean_test_reviews)
-    test_data_features = test_data_features.toarray()
-
-    # Use the random forest to make sentiment label predictions
-    print "Predicting test labels...\n"
-    result = forest.predict(test_data_features)
-
-    # Copy the results to a pandas dataframe with an "id" column and# Initialize the "CountVectorizer" object, which is scikit-learn's
-    # bag of words tool.
-    vectorizer = CountVectorizer(analyzer = "word",   \
-                             tokenizer = None,    \
-                             preprocessor = None, \
-                             stop_words = None,   \
-                             max_features = 5000)
-
-    # fit_transform() does two functions: First, it fits the model
-    # and learns the vocabulary; second, it transforms our training data
-    # into feature vectors. The input to fit_transform should be a list of
-    # strings.
-    train_data_features = vectorizer.fit_transform(clean_train_reviews)
-
-    # Numpy arrays are easy to work with, so convert the result to an
-    # array
-    train_data_features = train_data_features.toarray()
-
-    # ******* Train a random forest using the bag of words
-    #
-    print "Training the random forest (this may take a while)..."
+    model = model.fit(X, y)
 
 
-    # Initialize a Random Forest classifier with 100 trees
-    forest = RandomForestClassifier(n_estimators = 100)
-
-    # Fit the forest to the training set, using the bag of words as
-    # features and the sentiment labels as the response variable
-    #
-    # This may take a few minutes to run
-    forest = forest.fit( train_data_features, train["sentiment"] )
-
-
-
-    # Create an empty list and append the clean reviews one by one
-    clean_test_reviews = []
-
-    print "Cleaning and parsing the test set movie reviews...\n"
-    for i in xrange(0,len(test["review"])):
-        clean_test_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], True)))
-
-    # Get a bag of words for the test set, and convert to a numpy array
-    test_data_features = vectorizer.transform(clean_test_reviews)
-    test_data_features = test_data_features.toarray()
-
-    # Use the random forest to make sentiment label predictions
-    print "Predicting test labels...\n"
-    result = forest.predict(test_data_features)
-
-    # Copy the results to a pandas dataframe with an "id" column and
-    # a "sentiment" column
-    output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
-
-    # Use pandas to write the comma-separated output file
-    output.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'Bag_of_Words_model.csv'), index=False, quoting=3)
-    print "Wrote results to Bag_of_Words_model.csv"
-    # a "sentiment" column
-    output = pd.DataFrame(data={"id": test["id"], "sentiment": result})
-
-    # Use pandas to write the comma-separated output file
-    output.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'Bag_of_Words_model.csv'), index=False, quoting=3)
-    print "Wrote results to Bag_of_Words_model.csv"
 
