c0fcbeeed6a3a48b25575ccd43fae377bb574a81
Liran Markin <liran.markin@gmail.com>
2017-02-27 15:24:52 +0200

    scikit-learn SVM is being used, we can predict sentences

--- python_scripts/BagOfWords.py
+++ python_scripts/BagOfWords.py
@@ -2,54 +2,52 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
 
-def sentence_to_wordlist(sentence, remove_stopwords=False):
-    # Function to convert a document to a sequence of words,
-    # optionally removing stop words.  Returns a list of words.
-    #
-    # 1. Remove non-letters
-    sentence = re.sub("[^a-zA-Z]", " ", sentence)
-    #
-    # 2. Convert words to lower case and split them
-    words = sentence.lower().split()
-    #
-    # 3. Optionally remove stop words (false by default)
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in stops]
-    #
-    # 4. Return a list of words
-    return words
+
+class BagOfWords():
+    def __init__(self):
+        # Initialize the "CountVectorizer" object, which is scikit-learn's
+        # bag of words toolsentiment.
+        self.vectorizer = CountVectorizer(analyzer="word", \
+                                     tokenizer=None, \
+                                     preprocessor=None, \
+                                     stop_words=None, \
+                                     max_features=5000)
+
+    def sentence_to_wordlist(self, sentence, remove_stopwords=False):
+        # Function to convert a document to a sequence of words,
+        # optionally removing stop words.  Returns a list of words.
+        #
+        # 1. Remove non-letters
+        sentence = re.sub("[^a-zA-Z]", " ", sentence)
+        #
+        # 2. Convert words to lower case and split them
+        words = sentence.lower().split()
+        #
+        # 3. Optionally remove stop words (false by default)
+        if remove_stopwords:
+            stops = set(stopwords.words("english"))
+            words = [w for w in words if not w in stops]
+        #
+        # 4. Return a list of words
+        return words
 
 
 
-def clean_sentences(sentences):
-    # Initialize an empty list to hold the clean_sentences
-    clean = []
+    def clean_sentences(self, sentences):
+        # Initialize an empty list to hold the clean_sentences
+        clean = []
 
-    for i in xrange(0, len(sentences)):
-        clean.append(" ".join(sentence_to_wordlist(sentences[i], True)))
+        for i in xrange(0, len(sentences)):
+            clean.append(" ".join(self.sentence_to_wordlist(sentences[i], True)))
 
-    return clean
+        return clean
 
-def get_features(sentences):
+    def fit_transform(self, sentences):
 
-    sentences = clean_sentences(sentences)
-    # Initialize the "CountVectorizer" object, which is scikit-learn's
-    # bag of words toolsentiment.
-    vectorizer = CountVectorizer(analyzer="word", \
-                                 tokenizer=None, \
-                                 preprocessor=None, \
-                                 stop_words=None, \
-                                 max_features=5000)
+        sentences = self.clean_sentences(sentences)
+        return self.vectorizer.fit_transform(sentences).toarray()
 
-    # fit_transform() does two functions: First, it fits the model
-    # and learns the vocabulary; second, it transforms our training data
-    # into feature vectors. The input to fit_transform should be a list of
-    # strings.
-    train_data_features = vectorizer.fit_transform(sentences)
+    def transform(self, sentences):
 
-    # Numpy arrays are easy to work with, so convert the result to an
-    # array
-    train_data_features = train_data_features.toarray()
-
-    return train_data_features
+        sentences = self.clean_sentences(sentences)
+        return self.vectorizer.transform(sentences).toarray()
--- python_scripts/myscript.py
+++ python_scripts/myscript.py
@@ -2,8 +2,8 @@ import os
 
 import pandas as pd
 import numpy as np
-from BagOfWords import get_features
-from SVM import SVM
+from BagOfWords import BagOfWords
+from sklearn.svm import LinearSVC, SVC
 
 
 def read_data(path):
@@ -15,40 +15,61 @@ def get_sentences(data):
 def get_emotions(data):
     return data['emotion'].values
 
+def initialize_model():
+    # Initialize a SVM classifier
+    model = SVC(max_iter=1000, tol=0.0001, C=1.0, class_weight=None, verbose=0, random_state=None)
+    return model
+
+def fit_model(model, X, y):
+    # Fit the model using the bag of words
+    print "Fitting the model (this may take a while)..."
+
+    # Fit the SVM to the training set, using the bag of words as
+    # features and the sentiment labels as the response variable
+    #
+    # This may take a few minutes to run
+    model = model.fit(X,y)
+    return model
+
 if __name__ == '__main__':
 
     print "Reading data from file"
 
     train = read_data('./data/real_data.csv')
 
-    train = train[:100]
+    test = train[30000:40000]
+    train = train[:30000]
 
     sentences = get_sentences(train)
     emotions = get_emotions(train)
 
-    #Transforming emotions to integer values
-    emotion_list = list(set(emotions))
-    emotion_numbers = {}
-    for i in xrange(len(emotion_list)):
-        emotion_numbers[emotion_list[i]] = i
+    print "Press Enter to Continue..."
+    raw_input()
+
+    bag = BagOfWords()
+
+    X = bag.fit_transform(sentences)
+    #y = np.vectorize(emotion_numbers.get)(emotions)
+    y = emotions
+
+    model = initialize_model()
+    model = fit_model(model, X, y)
 
     print "Press Enter to Continue..."
     raw_input()
 
-    # Train a SVM using the bag of words
-    print "Training the SVM (this may take a while)..."
+    sentences = get_sentences(test)
+    emotions = get_emotions(test)
 
-    # Initialize a SVM classifier
-    model = SVM(max_iter=10000, kernel_type='linear', C=1.0, epsilon=0.001)
+    X = bag.transform(sentences)
+    y = emotions
 
-    X = get_features(sentences)
-    y = np.vectorize(emotion_numbers.get)(emotions)
+    print model.score(X, y)
 
-    # Fit the SVM to the training set, using the bag of words as
-    # features and the sentiment labels as the response variable
-    #
-    # This may take a few minutes to run
-    model = model.fit(X, y)
+
+
+
+
 
 
 
