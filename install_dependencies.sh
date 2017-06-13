11e4f530d4c2de19564e1a808b18651d208aee58
Liran Markin <liran.markin@gmail.com>
2017-06-13 16:54:30 +0300

    install script - python added

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,8 +3,8 @@ sudo apt-get install -y python python-pip npm
 sudo npm install -g pm2 n npm
 sudo n latest
 
-cd `dirname $0`/server
-npm install
-
 pip install -r requirements.txt
 python -m nltk.downloader all-corpora
+
+cd `dirname $0`/server
+npm install
